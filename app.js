4a8061a7a888e97a53072eb2804cd9467277e4ff
Jordan P <jperegrino@cryptocompare.com>
2017-10-09 13:04:29 +0000

    ready

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ angular.module('myApp', [])
 		$scope.currentType = { name: 'Line Chart', icon: 'fa-area-chart', extraClass: "pull-right", linkToPage: false, requiresLogin: false };
 		$scope.currentDataSet = [];
 		$scope.chartPeriod = [
-			{ name: '1 Hour', shortName: '1 H', maxPoints: 60, type: 'minute', aggregation: 1, periodForTable: 10, periodForTableName: '10 min', period: 'mm', polyfill: 60, requiresLogin: false, valueGridCount: 8, catGridCount: 10, balloonDateFormat: "JJ:NN" },
+			{ name: '1 Hour', shortName: '1 H', maxPoints: 60, type: 'minute', aggregation: 1, periodForTable: 10, periodForTableName: '10 min', period: 'mm', polyfill: 60, requiresLogin: false, valueGridCount: 7, catGridCount: 10, balloonDateFormat: "JJ:NN" },
 			{ name: '1 Day', shortName: '1 D', maxPoints: 144, type: 'minute', aggregation: 10, periodForTable: 18, periodForTableName: '3 hours', period: '10mm', polyfill: 10 * 60, requiresLogin: false, valueGridCount: 5, catGridCount: 8, balloonDateFormat: "JJ:NN" },
 			{ name: '1 Week', shortName: '1 W', maxPoints: 168, type: 'hour', aggregation: 1, periodForTable: 24, periodForTableName: 'day', period: 'hh', polyfill: 60 * 60, requiresLogin: false, valueGridCount: 5, catGridCount: 10, balloonDateFormat: "MMM DD, JJ:NN" },
 			{ name: '1 Month', shortName: '1 M', maxPoints: 120, type: 'hour', aggregation: 6, periodForTable: 12, periodForTableName: '3 days', period: '6hh', polyfill: 6 * 60 * 60, requiresLogin: false, valueGridCount: 5, catGridCount: 10, balloonDateFormat: "MMM DD" },
@@ -21,7 +21,7 @@ angular.module('myApp', [])
 			}
 			else {
 				$scope.currentPeriod = $scope.chartPeriod[periodId];
-				chartUtil.getHistoFromMinApi($scope.currentPeriod.type, $scope.currentPeriod.maxPoints, $scope.currentPeriod.aggregation,"BTC","USD").then(function(response) {
+				chartUtil.getHistoFromMinApi($scope.currentPeriod.type, $scope.currentPeriod.maxPoints, $scope.currentPeriod.aggregation,"ETH","BTC").then(function(response) {
 					$scope.currentDataSet = response.data.Data;
 					for (var index in $scope.currentDataSet) {
 						$scope.currentDataSet[index]['time'] = new Date($scope.currentDataSet[index]['time'] * 1000);
@@ -31,6 +31,7 @@ angular.module('myApp', [])
 
 			}
 		};
+		
 
 		$scope.generateNewChart = function(dataObject, currentPeriod) {
 
